061cfe71d9b68604c54c3f71df8a6518c2c1924a
Medhavi Srivastava <43985601+Medhavi-16@users.noreply.github.com>
2021-01-02 22:26:25 +0530

    basic setup files

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigation from './src/navigators/TabNavigation'
+
+export default function App() {
+  return (
+   <NavigationContainer>
+     <TabNavigation/>
+   </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
