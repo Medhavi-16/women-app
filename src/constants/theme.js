58d046890e48158706312b0d66aa34ba04a42a81
Medhavi Srivastava <43985601+Medhavi-16@users.noreply.github.com>
2021-01-02 22:33:23 +0530

    added src code

---
+++ src/constants/theme.js
@@ -0,0 +1,65 @@
+const colors = {
+    accent: '#f77c45',
+    primary: '#e4f5ea',
+    secondary: '#40987f',
+    tertiary: '#7fc0a8',
+    black: '#000000',
+    white: '#FFFFFF',
+    white2: 'rgba(255,255,255,0.5)',
+    gray: '#9DA3B4',
+    gray2: '#C5CCD6',
+    gray3: 'rgba(197, 204, 214, 0.4)',
+    red: '#FF0000',
+    lightGrey: '#f5f5f5',
+    lightPink: '#ffecf2',
+    redPink: '#f7536e',
+    pastelPink: '#ffd1dc',
+    lightLilac: '#ffefef',
+    cream: '#fdfff5',
+    brown: '#8d5845',
+    yellow: '#face1b',
+    darkGrey: '#7fc0a8'
+  };
+  
+  const sizes = {
+    // global sizes
+    base: 16,
+    font: 14,
+    radius: 6,
+    padding: 25,
+  
+    // font sizes
+    h1: 26,
+    h2: 20,
+    h3: 18,
+    title: 18,
+    header: 16,
+    body: 14,
+    caption: 12
+  };
+  
+  const fonts = {
+    h1: {
+      fontSize: sizes.h1
+    },
+    h2: {
+      fontSize: sizes.h2
+    },
+    h3: {
+      fontSize: sizes.h3
+    },
+    header: {
+      fontSize: sizes.header
+    },
+    title: {
+      fontSize: sizes.title
+    },
+    body: {
+      fontSize: sizes.body
+    },
+    caption: {
+      fontSize: sizes.caption
+    }
+  };
+  
+  export { colors, sizes, fonts };
---
+++ src/screens/HealthScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const HealthScreen=()=>{
+    return(
+       <View>
+           <Text>HealthScreen</Text>
+       </View> 
+    )
+}
+export default HealthScreen
+
+
+const styles = StyleSheet.create({
+    
+})
